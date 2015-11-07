3bbfcb39691ad50aebe9fec728814c6f929f20fe
Αθηνόδωρος <athinodwros89@gmail.com>
2015-11-07 21:07:44 +0200

    Ath -> add cout command to constructors and destructors

--- ClassesAndInheritanceTest1/Animal.cpp
+++ ClassesAndInheritanceTest1/Animal.cpp
@@ -8,7 +8,9 @@ using std::cout;
 Animal::Animal() : name("Animal"),age(0){
 	cout << "Constructing default animal" << endl;
 }
-Animal::Animal(string name, int age) : name(name),age(age){}
+Animal::Animal(string name, int age) : name(name),age(age){
+	cout << "Constructing animal with name:" << name << " age: " << age << endl;
+}
 Animal::~Animal(){
 	cout << "Destructing default animal" << endl;
 }
--- ClassesAndInheritanceTest1/Human.cpp
+++ ClassesAndInheritanceTest1/Human.cpp
@@ -1,7 +1,17 @@
 #include "stdafx.h"
 #include "Human.h"
+#include "iostream"
 
+using std::cout;
+using std::endl;
 
-Human::Human() : Animal("Human",18), iq(100), gender(Male){}
-Human::Human(string name, int age, int iq, Gender gender) : Animal(name,age),iq(iq),gender(gender){}
-Human::~Human(){}
+Human::Human() : Animal("Human",18), iq(100), gender(Male){
+	cout << "Constructing default human" << endl;
+}
+Human::Human(string name, int age, int iq, Gender gender) : Animal(name,age),iq(iq),gender(gender){
+	cout << "Constructing human with name:" << name << " age: " << age << " iq:" << iq << " gender:" << gender << endl;
+}
+
+Human::~Human(){
+	cout << "Destructing human" << endl;
+}
