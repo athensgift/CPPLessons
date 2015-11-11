7a5486b8d87734212010e43fe63a599b3bfe8c9d
Αθηνόδωρος <athinodwros89@gmail.com>
2015-11-11 15:57:04 +0200

    GradingProgramm completed

--- CppLessons/GradingProgramm/GradingProgramm.cpp
+++ CppLessons/GradingProgramm/GradingProgramm.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+	int grade = 0;
+	cout << "Please enter your integer grade: ";
+	cin >> grade;
+
+	string score;
+	if (grade >= 0 && grade <= 59) {
+		score = "F";
+	}
+	else if (grade <= 69) {
+		score = "D";
+	}
+	else if (grade <= 79) {
+		score = "C";
+	}
+	else if (grade <= 89) {
+		score = "B";
+	}
+	else if (grade <= 100) {
+		score = "A";
+	}
+	else {
+		cout << "You entered wrong Value BYE BYE";
+	}
+
+	cout << "Your score is :" + score;
+
     return 0;
 }
 
